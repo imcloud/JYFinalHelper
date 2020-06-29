1bb85938c4a56c6cea0a3947762e7c7dcc1d262f
suziye <240081368@qq.com>
2020-06-29 20:38:22 +0800

    create

---
+++ js/script.js
@@ -0,0 +1,69 @@
+﻿//enter回首頁
+document.onkeydown=function(){ss()}
+function ss()
+{
+ if(event.keyCode==13)
+ {
+  location="index.html";     
+ }
+}
+
+//去除連結虛線框
+$(document).ready(
+function(){
+      $('a').focus(
+function(){
+    $(this).blur();
+})
+    })
+
+//點擊隱藏顯示
+$(function(){
+		// 幫 #qaContent 的 ul 子元素加上 .accordionPart
+		// 接著再找出 li 中的第一個 div 子元素加上 .qa_title
+		// 並幫其加上 hover 及 click 事件
+		// 同時把兄弟元素加上 .qa_content 並隱藏起來
+		$('#qaContent ul').addClass('accordionPart').find('li div:nth-child(1)').addClass('qa_title').hover(function(){
+			$(this).addClass('qa_title_on');
+		}, function(){
+			$(this).removeClass('qa_title_on');
+		}).click(function(){
+			// 當點到標題時，若答案是隱藏時則顯示它，同時隱藏其它已經展開的項目
+			// 反之則隱藏
+			var $qa_content = $(this).next('div.qa_content');
+			if(!$qa_content.is(':visible')){
+				$('#qaContent ul li div.qa_content:visible').slideUp();
+			}
+			$qa_content.slideToggle();
+		}).siblings().addClass('qa_content').hide();
+	});
+
+//頁籤區塊
+$(function(){
+		// 預設顯示第一個 Tab
+		var _showTab = 0;
+		$('.abgne_tab').each(function(){
+			// 目前的頁籤區塊
+			var $tab = $(this);
+
+			var $defaultLi = $('ul.tabs li', $tab).eq(_showTab).addClass('active');
+			$($defaultLi.find('a').attr('href')).siblings().hide();
+			
+			// 當 li 頁籤被點擊時...
+			// 若要改成滑鼠移到 li 頁籤就切換時, 把 click 改成 mouseover
+			$('ul.tabs li', $tab).click(function() {
+				// 找出 li 中的超連結 href(#id)
+				var $this = $(this),
+					_clickTab = $this.find('a').attr('href');
+				// 把目前點擊到的 li 頁籤加上 .active
+				// 並把兄弟元素中有 .active 的都移除 class
+				$this.addClass('active').siblings('.active').removeClass('active');
+				// 淡入相對應的內容並隱藏兄弟元素
+				$(_clickTab).stop(false, true).fadeIn().siblings().hide();
+
+				return false;
+			}).find('a').focus(function(){
+				this.blur();
+			});
+		});
+	});
